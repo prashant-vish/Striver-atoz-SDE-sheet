08fe41fc07d6dff12b0bc0e9acad3e8434604a87
prashant <mohitvishwakarma24092001@gmail.com>
2025-02-13 11:09:50 +0530

    print n to 1  Without loop

---
+++ 11_printNto1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    void printNos(int N)
+    {
+        if (N == 0)
+        {
+            return;
+        }
+        cout << N << " ";
+        printNos(N - 1);
+    }
+};
