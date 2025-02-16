3b5ff895db5cf412860562a4d00d6ead6bcf9c8a
prashant <mohitvishwakarma24092001@gmail.com>
2025-02-16 19:54:12 +0530

    number of Subarray with sum k

---
+++ 35-type-B-numberOfSubarrayWithSumK.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+        int subarraySum(vector<int>& arr, int k) {
+    
+        int n=arr.size();
+        int sum=0;
+        int ans=0;
+        unordered_map<int, int> preSum;
+        for(int i=0;i<n;i++){
+            sum=sum+arr[i];
+            if(sum==k){
+                ans++;
+            }
+            int rem=sum-k;
+            if(preSum.find(rem)!=preSum.end()){
+                ans=ans+preSum[rem];
+            }
+            preSum[sum]=preSum[sum]+1;
+        }
+        return ans;
+        }
+    };
