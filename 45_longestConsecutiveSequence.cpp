e89fa819c73dab62abaccc8834a92f0dfe845045
prashant <mohitvishwakarma24092001@gmail.com>
2025-02-24 00:32:19 +0530

    Updated Brute force and added optimal

--- 45_longestConsecutiveSequence.cpp
+++ 45_longestConsecutiveSequence.cpp
@@ -7,34 +7,80 @@ using namespace std;
 
 // Sort the array.
 // Traverse the array and for each element, check if it is greater than the previous element.
-// If it is, then increment the count.  Else, update the maximum count found so far.
+// If it is, then increment the count.  Else, update the longestmum count found so far.
 // Return the maximum count found so far.
 
-class Solution {
-    public:
-        int longestConsecutive(vector<int>& nums) {
-    
-            sort(nums.begin(),nums.end());
-            int maxi=1;
-            int n=nums.size();
-            if(n==0) return 0;
-            int cnt=1;
-            for(int i=1;i<n;){
-                if(nums[i]-nums[i-1]==1){
-                    while(i<n && nums[i]-nums[i-1]==1){
-                        cnt++;
-                        i++;
-                    }
-                }
-                else if(nums[i]-nums[i-1]==0){
-                    i++;
-                }
-                else{
-                    cnt=1;
-                    i++;
-                }
-                maxi=max(cnt,maxi);
+class Solution
+{
+public:
+    int longestConsecutive(vector<int> &nums)
+    {
+
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int longest = 1;
+        int cnt = 0;
+        int lastSmaller = INT_MIN;
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] - 1 == 1)
+            {
+                cnt++;
+                lastSmaller = nums[i];
             }
-            return maxi;
+            else if (lastSmaller != nums[i])
+            {
+                cnt = 1;
+                lastSmaller = nums[i];
+            }
+            longest = max(cnt, longest);
         }
-    };
+        return longest;
+    }
+};
+
+// Optimal Solution
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+// Create a set and insert all the elements of the array into the set.
+// Traverse the array and for each element, check if it is the starting element of a sequence.
+// If it is, then increment the count and keep checking for the next elements in the sequence.
+// Update the longestmum count found so far.
+// Return the maximum count found so far.
+
+class Solution
+{
+public:
+    int longestConsecutive(vector<int> &nums)
+    {
+
+        int n = nums.size();
+        if (n == 0)
+            return 0;
+        sort(nums.begin(), nums.end());
+
+        int longest = 1;
+        unordered_set<int> st;
+
+        for (int i = 0; i < n; i++)
+        {
+            st.insert(nums[i]);
+        }
+        for (auto it : st)
+        {
+            if (st.find(it - 1) == st.end())
+            {
+                int cnt = 1;
+                int x = it;
+                while (st.find(x + 1) != st.end())
+                {
+                    x = x + 1;
+                    cnt++;
+                }
+                longest = max(cnt, longest);
+            }
+        }
+        return longest;
+    }
+};
