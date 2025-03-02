92c910939eadd937b7802f0c30d886ae28ec425b
prashant <mohitvishwakarma24092001@gmail.com>
2025-03-03 00:00:08 +0530

    Added Brute Force

--- 47_RotateBy90.cpp
+++ 47_RotateBy90.cpp
@@ -2,6 +2,24 @@
 using namespace std;
 
 
+// Brute Force 
+// Time complexity O(n*n)
+// Space Complexity O(n*n)
+
+class Solution {
+    public:
+        void rotate(vector<vector<int>>& matrix) {
+            int n = matrix.size();
+            vector<vector<int>> temp = matrix;
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    temp[j][n - i - 1] = matrix[i][j];
+                }
+            }
+            matrix = temp;
+        }
+};
+
 // Optimal Code 
 // Time complexity o(n*n)
 // Space Complexity O(1)
