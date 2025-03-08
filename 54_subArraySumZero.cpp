bb5f6f1851eba39230b29a36ff8c42fcbaa94eb8
prashant <mohitvishwakarma24092001@gmail.com>
2025-03-08 23:55:10 +0530

    added subArray sum with zero sum

---
+++ 54_subArraySumZero.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Brute force approach:
+// Time complexity: O(n^2)
+// Space complexity: O(1)
+
+// Optimized approach:
+// Time complexity: O(nlogn)
+// Space complexity: O(n)
+
+class Solution
+{
+public:
+    int maxLen(vector<int> &arr)
+    {
+        int maxi = 0;
+
+        unordered_map<int, int> mpp;
+        int sum = 0;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            sum += arr[i];
+
+            if (sum == 0)
+            {
+                maxi = i + 1;
+            }
+            else
+            {
+
+                if (mpp.find(sum) != mpp.end())
+                {
+                    maxi = max(maxi, i - mpp[sum]);
+                }
+                else
+                {
+                    mpp[sum] = i;
+                }
+            }
+        }
+        return maxi;
+    }
+};
