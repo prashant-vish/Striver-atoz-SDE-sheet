eff7639ed6999fa1aee4f1e2cb8bd867a56cae93
prashant <mohitvishwakarma24092001@gmail.com>
2025-03-15 23:47:43 +0530

    01 binary Search

---
+++ Binary Search/01_binarySeach.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int l = 0;
+        int h = nums.size() - 1;
+        int mid;
+        while (l <= h)
+        {
+            mid = (l + h) / 2;
+
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (nums[mid] < target)
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                h = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+ // Handle the case where k is large
+    // After n operations, the pattern might repeat
