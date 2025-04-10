8d761517386ce1150b72bf5a4e12ea186be43571
prashant <mohitvishwakarma24092001@gmail.com>
2025-04-10 12:35:35 +0530

    optimal solution for kth missinge element

--- Binary Search/19_kthMissingElement.cpp
+++ Binary Search/19_kthMissingElement.cpp
@@ -27,3 +27,48 @@ public:
         return arr[arr.size() - 1] + (k - total);
     }
 };
+
+// Another One
+class Solution
+{
+public:
+    int findKthPositive(vector<int> &arr, int k)
+    {
+
+        for (int i = 0; i < arr.size(); i++)
+        {
+            if (arr[i] <= k)
+                k++;
+            else
+                break;
+        }
+
+        return k;
+    }
+};
+
+// Optimal Solution
+// time Complexity - O(log(n))
+// space complexity -O(1)
+
+class Solution
+{
+public:
+    int findKthPositive(vector<int> &arr, int k)
+    {
+
+        int low = 0, high = arr.size() - 1;
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+
+            int missing = arr[mid] - (mid + 1);
+            if (missing < k)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return low + k;
+    }
+};
