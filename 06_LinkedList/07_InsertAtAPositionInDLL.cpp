fda69656cdc076c6fd5f8a6ec3f476c61e1ced67
prashant <mohitvishwakarma24092001@gmail.com>
2025-04-27 10:37:36 +0530

    Insert at a position in linked list

---
+++ 06_LinkedList/07_InsertAtAPositionInDLL.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    struct Node *prev;
+    Node(int x)
+    {
+        data = x;
+        next = prev = NULL;
+    }
+};
+
+// Function to insert a new node at given position in doubly linked list.
+class Solution
+{
+public:
+    // Function to insert a new node at given position in doubly linked list.
+    Node *addNode(Node *head, int pos, int data)
+    {
+
+        int count = 0;
+        Node *traverse = head;
+
+        while (count != pos)
+        {
+            traverse = traverse->next;
+            count++;
+        }
+        Node *newNode = new Node(data);
+        Node *nextwala = traverse->next;
+
+        traverse->next = newNode;
+        newNode->prev = traverse;
+        if (nextwala)
+        {
+            newNode->next = nextwala;
+            nextwala->prev = newNode;
+        }
+        return head;
+    }
