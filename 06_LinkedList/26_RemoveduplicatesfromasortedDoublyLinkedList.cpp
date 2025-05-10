a803deb0c423cc6eabab055b2f0f0310358bf8c6
prashant <mohitvishwakarma24092001@gmail.com>
2025-05-10 23:57:55 +0530

    Remove Duplicates from a Sorted DLL

---
+++ 06_LinkedList/26_RemoveduplicatesfromasortedDoublyLinkedList.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *prev;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+    Node(int data, Node *next, Node *prev)
+    {
+        this->data = data;
+        this->prev = prev;
+        this->next = next;
+    }
+};
+
+Node *removeDuplicates(Node *head)
+{
+    Node *temp = head;
+    while (temp)
+    {
+        Node *nextNode = temp;
+
+        while (nextNode != NULL &&temp->data = nextNode->data)
+        {
+            Node *duplicate = nextNode;
+            nextNode = nextNode->next;
+            delete duplicate;
+        }
+        temp->next = nextNode;
+        if (nextNode != NULL)
+            nextNode->prev = temp;
+        temp = temp->next;
+    }
+
+    return head;
+}
