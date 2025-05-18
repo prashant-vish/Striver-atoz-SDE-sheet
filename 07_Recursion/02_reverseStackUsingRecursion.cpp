4b65d1ed055be86fc6ab9c384839fd1035aefd12
prashant <mohitvishwakarma24092001@gmail.com>
2025-05-18 23:54:44 +0530

    reverse Stack using recursion

---
+++ 07_Recursion/02_reverseStackUsingRecursion.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertAtBottom(stack<int> &s, int x)
+{
+
+    if (s.size() == 0)
+    {
+        s.push(x);
+        return;
+    }
+
+    int value = s.top();
+    s.pop();
+    insertAtBottom(s, x);
+    s.push(value);
+}
+
+void reverseStack(stack<int> &stack)
+{
+
+    if (stack.empty())
+    {
+        return;
+    }
+    int value = stack.top();
+    stack.pop();
+    reverseStack(stack);
+    insertAtBottom(stack, value);
+}
