3fec0abc03bda1547fb06830994a26add7df87e5
prashant <mohitvishwakarma24092001@gmail.com>
2025-06-01 00:30:18 +0530

    Swap Two Number

--- 08_bit_manupulation/04_countTotalsebit.cpp
+++ 08_bit_manupulation/04_countTotalsebit.cpp
@@ -1,9 +1,14 @@
+
+
+
+
+// Brute force 
+// time complexity : n*2^31;
+
 class Solution {
+
   public:
-    // n: input to count the number of set bits
-    // Function to return sum of count of set bits in the integers from 1 to n.
     int countSetBits(int n) {
-        
         int sum=0;
         for(int i=1;i<=n;i++){
             int cnt=0;
@@ -12,7 +17,6 @@ class Solution {
                 cnt++;
             }
             sum+=cnt;
-        
     }
         
         return sum;
