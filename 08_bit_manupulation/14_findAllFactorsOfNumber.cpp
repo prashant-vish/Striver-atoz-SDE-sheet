91cbe684b18cc09cb615a74892d771d1b4555884
prashant <mohitvishwakarma24092001@gmail.com>
2025-06-05 23:44:59 +0530

    Find all factors of Number

---
+++ 08_bit_manupulation/14_findAllFactorsOfNumber.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// brute force 
+// time complexity - O(n)
+// space complexity - O(1)
+
+class Solution {
+  public:
+    void print_divisors(int n) {
+        
+        
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                cout<<i<<" ";
+            }
+        }
+        
+        return ;
+    }
+};
+
+// optimal solution 
+// time complexity - O(sqrt(N)+sqrt(N)log(sqrt(n)))
+// space complexity - O(sqrt(N))
+
+class Solution
+{
+public:
+    void print_divisors(int n)
+    {
+
+        vector<int> ans;
+        for (int i = 1; i * i <= n; i++)
+        {
+
+            if (n % i == 0)
+            {
+                ans.push_back(i);
+                if (i != n / i)
+                    ans.push_back(n / i);
+            }
+        }
+
+        sort(ans.begin(), ans.end());
+
+        for (int i = 0; i < ans.size(); i++)
+        {
+            cout << ans[i] << " ";
+        }
+
+        return;
+    }
+};
