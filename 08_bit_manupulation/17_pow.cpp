d9865199eb0fa4e767e29249eebe6b4ff13f2618
prashant <mohitvishwakarma24092001@gmail.com>
2025-06-08 23:52:48 +0530

    Power of x raise to N

---
+++ 08_bit_manupulation/17_pow.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double helper(double x, long n){
+        if(n==0){
+            return 1;
+        }
+        if(n<0){
+            return helper(1/x,-n);
+        }
+
+        if(n%2==0){
+            return helper(x*x,n/2);
+        }
+
+        return x*helper(x*x,(n-1)/2);;
+    }
+    double myPow(double x, int n) {
+    
+        return helper(x,long(n));
+    }
+};
